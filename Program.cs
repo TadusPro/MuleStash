42c4f585f7fdbaca8ac6dda5dd0e52328ac5151d
TadusPro <tadus@tadus.se>
2019-03-17 10:19:43 +0100

    created a dotnet project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MuleStash
+{
+    class Program
+    {
+        private const string Value = "Hello World!";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+        }
+    }
+}
