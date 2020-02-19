a75a4166a0aba814b6ddfa82a0f991acd04720e1
TadusPro <tadus@tadus.se>
2020-02-19 09:55:44 +0100

    idk xdd

--- Mulestash.cs
+++ Mulestash.cs
@@ -1,44 +1,64 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-
 namespace MuleStash
 {
     class Mulestash
     {
-        public Mule[] mules;
+        public List<Mule> mules = new List<Mule>();
+
         private void OnStartup()
         {
+            //mules.Add(RegisterMule());
+
 
         }
         public Mulestash()
         {
             this.OnStartup();
+            while (true)
+            {
+            Console.Write(">");
+            string input = Console.ReadLine();
+                if (input=="mulecount")
+                {
+                    Console.WriteLine();
+                }
+                else if(input=="help")
+                {
+                    Console.WriteLine("current commands help");
+                }
+
+                else
+                {
+                    Console.WriteLine("No command found, type help for commands");
+                }
+                    
+            }
 
         }
 
         public Mule RegisterMule()
         {
-            Mule newmule = new Mule()
-            {
-                name = Utitily.RandomString(9),
-                guid = Utitily.RandomString(12) + "@mule.zozk.org",
-                password = Utitily.RandomString(8)+ Utitily.Randomint(8) + "WEFHO"
-            };
+            Mule newmule = new Mule(Utitily.RandomString(6) + "@" + Utitily.RandomString(3) + "." + Utitily.RandomString(2), Utitily.RandomString(2));
 
-            string url = "https://realmofthemadgodhrd.appspot.com/account/register?guid=DDDDDDDD30A5B289EA856859A8&newGUID=" + newmule.guid + "&newPassword=" + newmule.password + "&name=" + newmule.name + "&isAgeVerified=1";
+            string url = "https://realmofthemadgodhrd.appspot.com/account/register?guid=DDDDDDDD30A5B289EA856859A8&newGUID=" + newmule.guid + "&newPassword=" + newmule.password+ "&isAgeVerified=1";
             string response = Utitily.WebGetRequest(url);
 
             if (response.StartsWith("<Success>"))
             {
+                newmule.valid = true;
                 return newmule;
 
             } else
             {
             Console.WriteLine(response);
-                return new Mule();
+                newmule.valid = false;
+                return newmule;
             }
         }
     }
